c350d78521011e5faf3b9fd06ad726ddd1e2c039
Aliaksandr Sazankou <sazankou.a@gmail.com>
2026-05-11 22:03:46 +0200

    Update service worker cache name and assets list

--- order/sw.js
+++ order/sw.js
@@ -1,10 +1,18 @@
-const CACHE_NAME = 'pizza-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './storage.js', './favicon.svg'];
+const CACHE_NAME = 'v2-pizza-voice';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/storage.js',
+    '/parser.js',
+    '/favicon.svg'
+];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
