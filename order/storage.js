f6f1e85f49d3b90de7fb293aa456193d9d7a8205
Aliaksandr Sazankou <sazankou.a@gmail.com>
2026-05-11 21:56:52 +0200

    Refactor storage.js to use saveToDB function

--- order/storage.js
+++ order/storage.js
@@ -1,17 +1,13 @@
-const DB_NAME = "PizzaPWA";
-const STORE_NAME = "orders";
-
-function saveOrder(orderData) {
-    const request = indexedDB.open(DB_NAME, 1);
+export const saveToDB = (data) => {
+    const request = indexedDB.open("PizzaStore", 1);
 
     request.onupgradeneeded = (e) => {
-        const db = e.target.result;
-        db.createObjectStore(STORE_NAME, { keyPath: "id", autoIncrement: true });
+        e.target.result.createObjectStore("orders", { keyPath: "id", autoIncrement: true });
     };
 
     request.onsuccess = (e) => {
         const db = e.target.result;
-        const tx = db.transaction(STORE_NAME, "readwrite");
-        tx.objectStore(STORE_NAME).add({ ...orderData, created: new Date() });
+        const tx = db.transaction("orders", "readwrite");
+        tx.objectStore("orders").add({ ...data, date: new Date() });
     };
-}
+};
