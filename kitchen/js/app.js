7a363cbb5e6fb9dc3d1cf9847d57a8f11fdded6e
Aliaksandr Sazankou <sazankou.a@gmail.com>
2026-05-25 10:19:43 +0200

    Add files via upload

--- kitchen/js/app.js
+++ kitchen/js/app.js
@@ -98,7 +98,11 @@ createApp({
                     { facingMode: "environment" },
                     { 
                         fps: 15,
-                        videoConstraints: { width: { ideal: 720 }, height: { ideal: 720 } },
+                        videoConstraints: { 
+                            facingMode: "environment",
+                            width: { ideal: 720 }, 
+                            height: { ideal: 720 } 
+                        },
                         qrbox: (viewfinderWidth, viewfinderHeight) => {
                             const minEdge = Math.min(viewfinderWidth, viewfinderHeight);
                             const size = Math.floor(minEdge * 0.7);
