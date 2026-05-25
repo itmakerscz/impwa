b74bb39528a183dd6e978cf8e130db1c18682e32
Aliaksandr Sazankou <sazankou.a@gmail.com>
2026-05-25 07:25:28 +0200

    Add files via upload

--- kitchen/sw.js
+++ kitchen/sw.js
@@ -12,7 +12,16 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(async (cache) => {
+            for (const asset of ASSETS) {
+                await cache.add(asset).catch(err => {
+                    console.error(`[ServiceWorker] Failed to cache resource: ${asset}`, err);
+                    throw err;
+                });
+            }
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
